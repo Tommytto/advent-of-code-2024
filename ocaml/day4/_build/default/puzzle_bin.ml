e3d76cf82ad4950fa84537aa49233a40ddb7649f
Timur Togyzbaev <tim.togyzbaev@gmail.com>
2024-12-06 18:39:49 +0200

    remove builds

--- ocaml/day4/_build/default/puzzle_bin.ml
+++
@@ -1,9 +0,0 @@
-let input = Utils.read_file "./input.txt"
-
-let () = 
-  Puzzle.solve_part1 input
-  |> Printf.printf "Part 1 result: %d\n"
-
-let () = 
-  Puzzle.solve_part2 input
-  |> Printf.printf "Part 2 result: %d\n"
